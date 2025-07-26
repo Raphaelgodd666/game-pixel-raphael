03ce1ab95de476a8c282c946ad95d8c0d4cdfd68
Raphaelgodd666 <rafaelali182@gmail.com>
2025-07-26 15:22:15 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'ArrowRight') {
+    nextSlide();
+  } else if (e.key === 'ArrowLeft') {
+    prevSlide();
+  }
+});
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.remove('active');
+    if (i === index) {
+      slide.classList.add('active');
+    }
+  });
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  showSlide(currentSlide);
+}
